bd3bfb0e77f0948081c225611bc22ac92f52f4c5
william.kn93@gmail.com <william.kn93@gmail.com@7ec860d5-2199-9f11-215f-bcb6d58b8e92>
2014-04-15 01:27:57 +0000

    Added saving of lat and long

--- trunk/src/gamefinder/Game.java
+++ trunk/src/gamefinder/Game.java
@@ -13,98 +13,117 @@ import com.googlecode.objectify.annotation.Id;
 
 @Entity
 public class Game<LatLng> {
-	@Id
-	private Long id;
-	private String sport;
-	private Calendar date;
-	private LatLng location;
-	private int hour_s;
-	private int min_s;
-	private String ampm_s;
-	private int hour_e;
-	private int min_e;
-	private String ampm_e;
-	private int numPlayers;
-	private int gameID;
-	private int numOfPlayers;
+    @Id
+    private Long id;
+    private String sport;
+    private Calendar date;
+    private double[] location = new double[2];
+    //location[0] = latitude
+    //location[1] = longitude
+    private String locationName;
+    private String locationAddress;
+    private int hour_s;
+    private int min_s;
+    private String ampm_s;
+    private int hour_e;
+    private int min_e;
+    private String ampm_e;
+    private int numPlayers;
+    private int gameID;
+    private int numOfPlayers;
+        
+    public static final Logger _log = Logger.getLogger(GameServlet.class.getName());
+    
+    public Game(){}
+    
+    public String getSport(){
+            return sport;
+    }
+    public void setSport(String sport){
+            this.sport = sport;
+    }
+    
+    public void setID(int id){
+            this.gameID = id;
+    }
+    public int getID(){
+            return gameID;
+    }
+    public void setStartTime(int hour, int min, String AMPM){
+            this.hour_s = hour;
+            this.min_s = min;
+            this.ampm_s = AMPM;
+            //date.set(Calendar.HOUR,hour);
+            //date.set(Calendar.MINUTE,min);
+            //date.set(Calendar.AM_PM,AMPM);
+    }
+    public String getStartTime(){
+            String startTime;
+            startTime = hour_s + min_s + ampm_s;
+            return startTime;
+    }
+    
+    public void setNumPlayers(int numPlayers){
+            this.numPlayers = numPlayers;
+    }
+    
+    public int getNumPlayers(){
+            return numPlayers;
+    }
+    
+    public void joinGame(){
+            this.numPlayers += 1;
+    }
+    
+    public void setEndTime(int hour, int min, String AMPM){
+            this.hour_e = hour;
+            this.min_e = min;
+            this.ampm_e = AMPM;
+    //      date.set(Calendar.HOUR,hour);
+    //      date.set(Calendar.MINUTE,min);
+    //      date.set(Calendar.AM_PM,AMPM);
+    }
+        
+    public String getEndTime(){
+            String endTime;
+            endTime = hour_e + min_e + ampm_e;
+            return endTime;
+    }
+    
+    
+    public Calendar getDate(){
+            return date;
+    }
+    public int getNumOfPlayers(){
+            return numOfPlayers;
+    }
+    public void setNumOfPlayers(int players){
+            this.numOfPlayers=players;
+    }
 
-	
-	public static final Logger _log = Logger.getLogger(GameServlet.class.getName());
-	
-	public Game(){}
-	
-	public String getSport(){
-		return sport;
-	}
-	public void setSport(String sport){
-		this.sport = sport;
-	}
-	
-	public void setID(int id){
-		this.gameID = id;
-	}
-	public int getID(){
-		return gameID;
-	}
-	public void setStartTime(int hour, int min, String AMPM){
-		this.hour_s = hour;
-		this.min_s = min;
-		this.ampm_s = AMPM;
-		//date.set(Calendar.HOUR,hour);
-		//date.set(Calendar.MINUTE,min);
-		//date.set(Calendar.AM_PM,AMPM);
-	}
-	public String getStartTime(){
-		String startTime;
-		startTime = hour_s + min_s + ampm_s;
-		return startTime;
-	}
-	
-	public void setNumPlayers(int numPlayers){
-		this.numPlayers = numPlayers;
-	}
-	
-	public int getNumPlayers(){
-		return numPlayers;
-	}
-	
-	public void joinGame(){
-		this.numPlayers += 1;
-	}
-	
-	public void setEndTime(int hour, int min, String AMPM){
-		this.hour_e = hour;
-		this.min_e = min;
-		this.ampm_e = AMPM;
-	//	date.set(Calendar.HOUR,hour);
-	//	date.set(Calendar.MINUTE,min);
-	//	date.set(Calendar.AM_PM,AMPM);
-	}
-	
+    public double[] getLocation(){    
+            return location;
+    }
+    
+    public void setLocation(double lat, double lng){
+    	location[0] = lat;
+    	location[1] = lng;
+    }
+    
+    public void setLocationName(String name){
+    	locationName = name;
+    }
+    
+    public String getLocationName(){
+    	return locationName;
+    }
+    
+    public void setLocationAddress(String address){
+    	locationAddress = address;
+    }
 
-	
-	public String getEndTime(){
-		String endTime;
-		endTime = hour_e + min_e + ampm_e;
-		return endTime;
-	}
-	
-	
-	public Calendar getDate(){
-		return date;
-	}
-	public int getNumOfPlayers(){
-		return numOfPlayers;
-	}
-	public void setNumOfPlayers(int players){
-		this.numOfPlayers=players;
-	}
-
-
-	public LatLng getLocation(){
-		
-		return location;
-	}
-
-	
+    public String getLocationAddress(){
+    	return locationAddress;
+    }
+        
 }
