7ad79405c7309c0680d1fa2d0be3be6d1e52f009
william.kn93 <williamkn93@users.noreply.github.com>
2014-04-25 01:27:13 +0000

    added handling of dates and times

--- src/gamefinder/GameServlet.java
+++ src/gamefinder/GameServlet.java
@@ -1,6 +1,7 @@
 package gamefinder;
 
 import java.io.*;
+import java.util.Calendar;
 import java.util.logging.Logger;
 
 import javax.servlet.http.*;
@@ -24,15 +25,36 @@ public class GameServlet extends HttpServlet {
 	public static final Logger _log = Logger.getLogger(GameServlet.class.getName());
 	
     @SuppressWarnings("rawtypes")
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {	
+    	boolean validGame = true;
+    	
+    	
     	double latitude = 0;
 	    double longitude = 0;
+	    
+        try{
+  	       latitude = Double.parseDouble(req.getParameter("latitude"));
+  	       longitude = Double.parseDouble(req.getParameter("longitude"));
+         }
+         catch(NumberFormatException e){
+        	validGame = false;
+         	_log.info("Error - no location picked");
+      	   	resp.setContentType("text/html");
+     		PrintWriter output = resp.getWriter();
+      	    String res = "Error: no location chosen on map!";
+  		    output.println(
+  		    "<!doctype html public \"-//w3c//dtd html 4.0 " +
+  		    "transitional//en\">\n" +
+  		    "<html>\n" +
+  		    "<center>" + res + "</center>\n" +
+  		    "</body></html>");
+         	resp.sendRedirect("/home.jsp");
+         	return;
+         }
+	    
         String sportName = req.getParameter("sport");
-        //String beginTime = req.getParameter("beginTime");
         String beginAMorPM = req.getParameter("beginAMorPM");
         String endAMorPM = req.getParameter("endAMorPM");
-        //String endTime = req.getParameter("endTime");
 
         //Long gameID = Long.parseLong(req.getParameter("id"));
       
@@ -50,27 +72,50 @@ public class GameServlet extends HttpServlet {
         int maxPlayers = Integer.parseInt(req.getParameter("numOfPlayers"));
         Boolean emailAlert = Boolean.parseBoolean(email);
         //Boolean smsAlert = Boolean.parseBoolean(sms);
-        int year = Integer.parseInt(req.getParameter("Year"));
-        int month = Integer.parseInt(req.getParameter("Month"));
-        int day = Integer.parseInt(req.getParameter("Day"));
+        
+
+    	int year = Integer.parseInt(req.getParameter("Year"));
+    	int month = Integer.parseInt(req.getParameter("Month"));
+    	int day = Integer.parseInt(req.getParameter("Day"));
+        
+        
         String locationName = req.getParameter("locationName");
 
-        try{
- 	       latitude = Double.parseDouble(req.getParameter("latitude"));
- 	       longitude = Double.parseDouble(req.getParameter("longitude"));
-        }
-        catch(NumberFormatException e){
-        	_log.info("Error - no location picked");
-     	   	resp.setContentType("text/html");
-    		PrintWriter output = resp.getWriter();
-     	    String res = "Error: no location chosen on map!";
- 		    output.println(
- 		    "<!doctype html public \"-//w3c//dtd html 4.0 " +
- 		    "transitional//en\">\n" +
- 		    "<html>\n" +
- 		    "<center>" + res + "</center>\n" +
- 		    "</body></html>");
+    	//checking if the game date is before current date
+    	Calendar current = Calendar.getInstance();
+    	Calendar gameCal = Calendar.getInstance();
+    	gameCal.set(year, month-1, day);
+    	int beginTimeHour_24 = beginTimeHour;
+    	if(beginTimeHour==12 && beginAMPM.equals("AM")){
+    		beginTimeHour_24 = 0;
+    		//why you playing at midnight??
+    	}
+    	else if(beginAMPM.equals("PM") && beginTimeHour!=12){
+    		beginTimeHour_24 = 12+beginTimeHour;
+    	}
+        gameCal.set(Calendar.HOUR_OF_DAY, beginTimeHour_24);
+        gameCal.set(Calendar.MINUTE, beginTimeMin);
+        if(current.after(gameCal)){
+        	_log.info("Game is set to a previous date...");
+        	_log.info("current time is "+current.getTime());
+        	_log.info("game time is"+gameCal.getTime());
         	resp.sendRedirect("/home.jsp");
+        	return;
+        }
+        int endTimeHour_24=0;
+        if(endTimeHour==12 && endAMPM.equals("AM")){
+    		endTimeHour_24 = 0;
+    	}
+    	else if(endAMPM.equals("PM") && endTimeHour!=12){
+    		endTimeHour_24 = 12+endTimeHour;
+    	}
+        endTimeHour_24=endTimeHour*60+endTimeMin;
+        beginTimeHour_24=beginTimeHour*60+beginTimeMin;
+        if(beginTimeHour_24 <= endTimeHour_24){
+        	validGame = false;
+        	_log.info("invalid game duration");
+        	resp.sendRedirect("/home.jsp");
+        	return;
         }
 
        Game game = new Game();
@@ -90,11 +135,16 @@ public class GameServlet extends HttpServlet {
 
         resp.sendRedirect("/home.jsp");
 
-       ofy().save().entity(game);
+
 
        _log.info("Game from " + beginTimeHour +":" + beginTimeMin + " until " + endTimeHour + ":" + endTimeMin);
-
-        _log.info("New game created!");
+       if(validGame){
+    	  ofy().save().entity(game);
+    	  _log.info("New game created!!"); 
+       }
+       else{
+    	   _log.info("Game not saved");
+       }
     }
     @SuppressWarnings("rawtypes")
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
